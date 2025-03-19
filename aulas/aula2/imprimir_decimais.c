7cb3a8c2fe8adbd128ad01335ac7ff64db48cd69
unknown <2512082024@academico.iesb.br>
2025-03-19 08:27:12 -0300

    mudanças

---
+++ aulas/aula2/imprimir_decimais.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("%f\n", 8.5);
+    printf ("%f %f\n", 9.3, 3.14);
+    printf("%.1f\n", 8.5);
+    printf("%.2f\n", 8.5);
+    return 0;
+}
---
+++ aulas/aula2/imprimir_inteiros.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+ printf("%i\n", 123);
+ printf("%i %i", 100, 200);
+ return 0;
+}
