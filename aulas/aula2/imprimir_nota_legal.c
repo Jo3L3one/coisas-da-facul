1eaabe2b565cf3eebb04f404c400571de524c08b
unknown <2512082024@ACADEMICO.IESB.BR>
2025-03-19 09:56:00 -0300

    s

--- aulas/aula2/imprimir_nota_legal.c
+++ aulas/aula2/imprimir_nota_legal.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("--------------------------------\n");
+    printf("-----------------------------------\n");
     printf("      N O T A     L E G A L     \n");
-    printf("--------------------------------\n");
-    printf("item          qde    prc   valor\n");
-    printf("%s %i %f %f\n", "Caneta Azul", 2, 2.0, 4.0);
-    printf("%s %i %f %f\n", "borracha", 1, 5.0, 5.0);
-    printf("%s %i %f %f\n", "resma de papel", 1, 12.0, 12.0);
-    printf("--------------------------------\n");
+    printf("-----------------------------------\n");
+    printf("    item         qde    prc   valor\n");
+    printf("%-15s %3i %7.2f %7.2f\n", "Caneta Azul   ", 2, 2.0, 4.0);
+    printf("%-15s %3i %7.2f %7.2f\n", "borracha  ", 1, 5.0, 5.0);
+    printf("%-15s %3i %7.2f %7.2f\n", "resma de papel", 1, 12.0, 12.0);
+    printf("-----------------------------------\n");
     printf("TOTAL.............:R$ 10b e 4,00\n");
 
     return 0;
