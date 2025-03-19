55b7924f1051144a0e582260e7480e7775014afb
unknown <2512082024@ACADEMICO.IESB.BR>
2025-03-19 08:51:35 -0300

    A

---
+++ aulas/aula2/imprimir_nota_legal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("--------------------------------\n");
+    printf("      N O T A     L E G A L     \n");
+    printf("--------------------------------\n");
+    printf("item          qde    prc   valor\n");
+    printf("caneta azul   002     2      4  \n");
+    printf("biorradiação  157     10     10b\n");
+    printf("--------------------------------\n");
+    printf("TOTAL.............:R$ 10b e 4,00\n");
+
+    return 0;
+}
+
