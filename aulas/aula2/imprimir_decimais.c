7797b53f72877f47b10271569283d91ea085d6d6
unknown <2512082024@ACADEMICO.IESB.BR>
2025-03-19 09:30:01 -0300

    a

--- aulas/aula2/imprimir_decimais.c
+++ aulas/aula2/imprimir_decimais.c
@@ -6,5 +6,6 @@ int main(){
     printf ("%f %f\n", 9.3, 3.14);
     printf("%.1f\n", 8.5);
     printf("%.2f\n", 8.5);
+    printf("%.0f\n", 7.65);
     return 0;
 }
--- aulas/aula2/imprimir_nota_legal.c
+++ aulas/aula2/imprimir_nota_legal.c
@@ -5,11 +5,11 @@ int main() {
     printf("      N O T A     L E G A L     \n");
     printf("--------------------------------\n");
     printf("item          qde    prc   valor\n");
-    printf("caneta azul   002     2      4  \n");
-    printf("biorradiação  157     10     10b\n");
+    printf("%s %i %f %f\n", "Caneta Azul", 2, 2.0, 4.0);
+    printf("%s %i %f %f\n", "borracha", 1, 5.0, 5.0);
+    printf("%s %i %f %f\n", "resma de papel", 1, 12.0, 12.0);
     printf("--------------------------------\n");
     printf("TOTAL.............:R$ 10b e 4,00\n");
 
     return 0;
 }
-
