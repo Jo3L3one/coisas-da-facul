0318785bcadaa388b6b2fa8fa97199e7e05f9f5d
Gabriel <gabrielfms2006@gmail.com>
2025-05-15 16:14:31 -0300

    ag

--- aulas/aula6/posso_votar.c
+++ aulas/aula6/posso_votar.c
@@ -5,7 +5,7 @@ int main() {
     int idade = 0;
 
     printf("informe sua idade: ");
-    scanf("%i!", &idade);
+    scanf("%i", &idade);
 
     if(idade < 16) {
         printf("vc nem pode votar, seja feliz");
